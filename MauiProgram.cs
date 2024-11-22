0b116d8e0c7e4c2aa9b6b517ef1e726e7a83c71d
rubyarellano <rubyorbasearellano@gmail.com>
2024-11-22 09:23:50 +0800

    Update

---
+++ MauiProgram.cs
@@ -0,0 +1,20 @@
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using CommunityToolkit.Maui;
+namespace SimpleApp;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+        return builder.Build();
+    }
+}
